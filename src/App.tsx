fad7f7feeff4254ea87d2c4158ca6aadbb0bff43
AlexP <alexander.pristavka@gmail.com>
2024-09-07 14:30:30 +0300

    Format code with prettier command

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,17 @@ import ClickCounter from './components/ClickCounter';
 const App = () => {
   // const  {name} = process.env;
   // console.log('=====================', JSON.stringify(process.env.name,null,2));
-  return <>
-    <h1>Big App component - {process.env.NODE_ENV} {process.env.name} {process.env.TEMP}</h1>
-    <img src={IMAGE} alt="React logo" width="200" height="200"/>
-    <img src={LOGO} alt="React logo svg" width="200" height="200"/>
-    <ClickCounter/>
-  </>
-}
+  return (
+    <>
+      <h1>
+        Big App component - {process.env.NODE_ENV} {process.env.name}{' '}
+        {process.env.TEMP}
+      </h1>
+      <img src={IMAGE} alt="React logo" width="200" height="200" />
+      <img src={LOGO} alt="React logo svg" width="200" height="200" />
+      <ClickCounter />
+    </>
+  );
+};
 
 export default App;
